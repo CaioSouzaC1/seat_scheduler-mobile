3997cba201bc9e0b28e1ce042adaa23b15bc8a12
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-08-06 23:44:57 -0300

    feat(homepage): adding button + form field

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:seat_scheduler_mobile/models/login_model.dart';
+import 'package:seat_scheduler_mobile/repositories/login_repository.dart';
+import 'package:seat_scheduler_mobile/repositories/login_repository_impl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,13 +11,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final LoginRepository loginRepository = LoginRepositoryImpl();
+  LoginModel? loginModel;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Fazer Login'),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(),
+              ElevatedButton(onPressed: () {}, child: const Text("Login"))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/repositories/login_repository.dart
+++ lib/repositories/login_repository.dart
@@ -1,5 +1,3 @@
-import 'package:seat_scheduler_mobile/models/login_model.dart';
-
-abstract interface class LoginRepository {
-  Future<LoginModel> makeLogin(String email, String password) {}
+abstract class LoginRepository {
+  Future<dynamic> makeLogin(String email, String password);
 }
