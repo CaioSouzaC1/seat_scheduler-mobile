8e3a1c39568084081891f6b51698d01b57bc2349
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-08-21 23:14:28 -0300

    chore(homepage): mock information

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,13 +11,42 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Colors.black87,
-            title: const Center(
-                child: Text(
-              "Hello, World",
-              style: TextStyle(color: Colors.white),
-            ))),
-        body: const Text("Home page"));
+      appBar: AppBar(
+        title: const Text("Main"),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const Text(
+              "Dados",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ListTile(
+              title: const Text("Nome"),
+              trailing: Text("John doe"),
+            ),
+            ListTile(
+              title: Text("Celular"),
+              trailing: Text("12312313"),
+            ),
+            ListTile(
+              title: Text("Email"),
+              trailing: Text("johndoe@gmail.com"),
+            ),
+            ListTile(
+              title: Text("Criação de conta"),
+              trailing: Text("19/08/2024"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
